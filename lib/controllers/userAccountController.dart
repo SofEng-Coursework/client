fd372b5ccaeaa686971bac5412f5beeb279f0a89
Elias <darkcornerz2025@gmail.com>
2024-04-29 14:39:07 +0100

    User push notifications

--- lib/controllers/userAccountController.dart
+++ lib/controllers/userAccountController.dart
@@ -8,6 +8,36 @@ import 'package:virtual_queue/controllers/FirebaseProvider.dart';
 
 class UserAccountController extends AccountController {
   late FirebaseProvider _firebaseProvider;
+
+  bool _pushNotificationEnabled = true;
+  bool _chatNotificationEnabled = true;
+  bool _emailNotificationEnabled = false;
+
   UserAccountController({required FirebaseProvider firebaseProvider})
     : super(collectionName: 'users', firebaseProvider: firebaseProvider);
+
+
+  bool get pushNotificationEnabled => _pushNotificationEnabled;
+  bool get chatNotificationEnabled => _chatNotificationEnabled;
+  bool get emailNotificationEnabled => _emailNotificationEnabled;
+
+// Methods to toggle notification settings
+  void togglePushNotification(bool value) {
+    _pushNotificationEnabled = value;
+    // Add logic to update push notification settings in the database
+    notifyListeners();
+  }
+
+  void toggleChatNotification(bool value) {
+    _chatNotificationEnabled = value;
+    // Add logic to update chat notification settings in the database
+    notifyListeners();
+  }
+
+  void toggleEmailNotification(bool value) {
+    _emailNotificationEnabled = value;
+    // Add logic to update email notification settings in the database
+    notifyListeners();
+  }
 }
+
