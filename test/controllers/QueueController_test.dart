d8dd3d20f5b2a9bed3dc0b6b69f724446d77c991
andero2003 <andero2003@gmail.com>
2024-05-01 09:55:20 +0100

    QueueController start

--- test/controllers/QueueController_test.dart
+++ test/controllers/QueueController_test.dart
@@ -1,6 +1,7 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:virtual_queue/controllers/AdminQueueController.dart';
 import 'package:virtual_queue/controllers/FirebaseProvider.dart';
 import 'package:virtual_queue/controllers/UserQueueController.dart';
 import 'package:virtual_queue/pages/RegisterForm.dart';
@@ -17,10 +18,7 @@ void main() {
       firebaseProvider = FirebaseProvider();
       firebaseProvider.initializeMock();
       userQueueController = UserQueueController(firebaseProvider: firebaseProvider);
-    });
-
-    setUp(() {
-      firebaseProvider.FIREBASE_AUTH.signOut();
+      firebaseProvider.FIREBASE_AUTH.signInWithEmailAndPassword(email: 'test', password:'test');
     });
 
     test('is initialized', () {
@@ -29,12 +27,13 @@ void main() {
 
     test('join and leave queue', () async {
       // Creating a mock queue
-      final fakeQueue = Queue(id: '1', name: 'test', open: true, users: []);
+      final fakeQueue = Queue(id: '1', name: 'test', open: true, users: [], logs: []);
       await firebaseProvider.FIREBASE_FIRESTORE.collection('queues').doc('1').set(fakeQueue.toJson());
 
       // Joining the queue
       final joinResult = await userQueueController.joinQueue(fakeQueue);
-      expect(joinResult, isNull);
+      print(joinResult.message);
+      expect(joinResult.success, true);
 
       // Getting current queue
       final currentQueue = await userQueueController.getCurrentQueue().first;
@@ -47,13 +46,13 @@ void main() {
 
       // Leaving the queue
       final leaveResult = await userQueueController.leaveQueue(currentQueue);
-      expect(leaveResult, isNull);
+      expect(leaveResult.success, true);
     });
 
     test('get queues', () async {
       // Creating some mock queues
-      final fakeQueue1 = Queue(id: '1', name: 'test1', open: true, users: []);
-      final fakeQueue2 = Queue(id: '2', name: 'test2', open: true, users: []);
+      final fakeQueue1 = Queue(id: '1', name: 'test1', open: true, users: [], logs: []);
+      final fakeQueue2 = Queue(id: '2', name: 'test2', open: true, users: [], logs: []);
       await firebaseProvider.FIREBASE_FIRESTORE.collection('queues').doc('1').set(fakeQueue1.toJson());
       await firebaseProvider.FIREBASE_FIRESTORE.collection('queues').doc('2').set(fakeQueue2.toJson());
 
@@ -64,6 +63,45 @@ void main() {
       expect(queues.length, 2);
       expect(queues[0].id, '1');
       expect(queues[1].id, '2');
+    });
+  });
+
+  group("AdminQueueController", () {
+    late UserQueueController userQueueController;
+    late AdminQueueController adminQueueController;
+    late FirebaseProvider firebaseProvider;
+
+    setUpAll(() {
+      firebaseProvider = FirebaseProvider();
+      firebaseProvider.initializeMock();
+      userQueueController = UserQueueController(firebaseProvider: firebaseProvider);
+      adminQueueController = AdminQueueController(firebaseProvider: firebaseProvider);
+    });
+
+    setUp(() {
+      firebaseProvider.FIREBASE_AUTH.signOut();
+    });
+
+    test('is initialized', () {
+      expect(adminQueueController, isA<AdminQueueController>());
+    });
+
+    test('add and delete queue', () async {
+      // Adding a queue
+      final addResult = await adminQueueController.addQueue('test', 10, '1');
+      expect(addResult.success, true);
+
+      // Getting queues
+      final queuesStream = adminQueueController.getQueues();
+      final queues = await queuesStream.first;
+      expect(queues, isA<List<Queue>>());
+      expect(queues.length, 1);
+      expect(queues[0].name, 'test');
+
+      // Deleting the queue
+      final deleteResult = await adminQueueController.deleteQueue(queues[0]);
+      expect(deleteResult.success, true);
+      expect(queues.length, 0);
     });
   });
 }
